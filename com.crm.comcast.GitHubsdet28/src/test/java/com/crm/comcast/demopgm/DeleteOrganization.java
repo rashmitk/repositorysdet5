f65d0ad958e6fff8fa378f25c61fb018c39db0b8
navee <navee@DESKTOP-8MA3HS2>
2022-01-22 12:02:55 +0530

    Modifications in DeleteOrganization

--- com.crm.comcast.GitHubsdet28/src/test/java/com/crm/comcast/demopgm/DeleteOrganization.java
+++ com.crm.comcast.GitHubsdet28/src/test/java/com/crm/comcast/demopgm/DeleteOrganization.java
@@ -4,6 +4,7 @@ public class DeleteOrganization {
 	
 	public static void main(String[] args) {
 		System.out.println("Organization is deleted successfully");
+		System.out.println("Modifications by TE1");
 		
 	}
 	
