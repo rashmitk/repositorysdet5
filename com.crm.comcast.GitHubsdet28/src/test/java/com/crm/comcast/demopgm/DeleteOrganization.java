663dd9d63a40bf9c8a9cca0e3c91128bd279d38f
navee <navee@DESKTOP-8MA3HS2>
2022-01-22 11:38:05 +0530

    second push of framework

---
+++ com.crm.comcast.GitHubsdet28/src/test/java/com/crm/comcast/demopgm/DeleteOrganization.java
@@ -0,0 +1,10 @@
+package com.crm.comcast.demopgm;
+
+public class DeleteOrganization {
+	
+	public static void main(String[] args) {
+		System.out.println("Organization is deleted successfully");
+		
+	}
+	
+}
